717f1223ab4cf4ca4054589720a25eeac1f89336
Ernst <rasmusernst@gmail.com>
2017-04-06 11:14:38 +0200

    added usermodels and routes

--- server.js
+++ server.js
@@ -6,12 +6,14 @@ const app = express();
 
 mongoose.connect("mongodb://localhost/assignment1");
 mongoose.Promise = global.Promise;
-
 app.use(bodyParser.json());
+app.use('/api', require('./api'));
 
-app.use(function(err,req,res,next){
+
+
+/*app.use(function(err,req,res,next){
     res.status(422).send({error:err.message});
-});
+});*/
 
 app.listen(process.env.port || 4000,function(){
     console.log('The server is now listening for requests');
